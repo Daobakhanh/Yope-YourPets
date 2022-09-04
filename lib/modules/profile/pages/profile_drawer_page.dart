1c2f9f8557323808020063bd98a6022c1e092203
Đào Bá Khánh <daobakhanhbk@gmail.com>
2022-09-04 16:43:40 +0700

    Add Alert Diolog feature coming soon

--- lib/modules/profile/pages/profile_drawer_page.dart
+++ lib/modules/profile/pages/profile_drawer_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yope_yourpet_social_networking/modules/auth/utils/auth_show_diolog_utils.dart';
 import 'package:yope_yourpet_social_networking/modules/profile/widgets/personal_profile_widget.dart';
 
 class PersonalProfileDrawerPage extends StatefulWidget {
@@ -21,54 +22,53 @@ class _PersonalProfileDrawerPageState extends State<PersonalProfileDrawerPage> {
           const SizedBox(
             height: 100,
           ),
-          // TextButton.icon(
-          //   onPressed: () {},
-          //   icon: const Icon(Icons.settings),
-          //   label: const Text(
-          //     'Settings',
-          //   ),
-          // ),
-          // TextButton.icon(
-          //   onPressed: () {},
-          //   icon: const Icon(UniconsLine.save),
-          //   label: const Text(
-          //     'Saved',
-          //   ),
-          // )
           TextIconButton(
             onTap: () {
-              debugPrint('Drawer ontap');
+              debugPrint('Drawer ontap Settings');
+              showMyDialog(context);
             },
             title: 'Settings',
             icon: Icons.settings,
           ),
           TextIconButton(
             onTap: () {
-              debugPrint('Drawer ontap');
+              debugPrint('Drawer ontap Settings');
+              showMyDialog(context);
             },
             title: 'Saved',
             icon: Icons.save,
           ),
           TextIconButton(
             onTap: () {
-              debugPrint('Drawer ontap');
+              debugPrint('Drawer ontap Activity');
+              showMyDialog(context);
             },
             title: 'Your Activity',
             icon: Icons.timelapse,
           ),
           TextIconButton(
             onTap: () {
-              debugPrint('Drawer ontap');
+              debugPrint('Drawer ontap Close Friends');
+              showMyDialog(context);
             },
             title: 'Close Friends',
             icon: Icons.person_off,
           ),
           TextIconButton(
             onTap: () {
-              debugPrint('Drawer ontap');
+              debugPrint('Drawer ontap Favorites');
+              showMyDialog(context);
             },
             title: 'Favorites',
             icon: Icons.star,
+          ),
+          TextIconButton(
+            onTap: () {
+              debugPrint('Drawer ontap Logout');
+              showMyDialog(context);
+            },
+            title: 'Logout',
+            icon: Icons.logout,
           )
         ],
       ),
