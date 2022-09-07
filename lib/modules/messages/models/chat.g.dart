e0fbe46db4ba49771b9ad362762392b9e32c7649
Đào Bá Khánh <daobakhanhbk@gmail.com>
2022-09-07 14:27:06 +0700

    custom chat mock data like real data, change folder of chat model

--- lib/modules/messages/models/chat.g.dart
+++ lib/modules/messages/models/chat.g.dart
@@ -7,12 +7,14 @@ part of 'chat.dart';
 // **************************************************************************
 
 Chat _$ChatFromJson(Map<String, dynamic> json) => Chat(
-      id: json['id'] as String,
-      text: json['text'] as String,
-      user: User.fromJson(json['user'] as Map<String, dynamic>),
-      replyCount: json['reply_count'] as int,
-      unreadCount: json['unread_count'] as int,
-      createdAt: json['created_at'] as String,
+      id: json['id'] as String?,
+      text: json['text'] as String?,
+      user: json['user'] == null
+          ? null
+          : User.fromJson(json['user'] as Map<String, dynamic>),
+      replyCount: json['reply_count'] as int?,
+      unreadCount: json['unread_count'] as int?,
+      createdAt: json['created_at'] as String?,
     );
 
 Map<String, dynamic> _$ChatToJson(Chat instance) => <String, dynamic>{
